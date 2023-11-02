869089ac87684a892c7873d8277e35d41be193a9
Alex Grasley <grasley.pdx@gmail.com>
2023-11-02 13:21:42 -0700

    add db tables

--- server/src/db/init.ts
+++ server/src/db/init.ts
@@ -1,9 +1,42 @@
 import { Database } from "sqlite3";
 import { open } from "sqlite";
+import * as path from "path";
+import { unlink, existsSync } from "fs";
+
+const p = path.join(__dirname, "database.db");
+if (existsSync(p)) {
+  unlink(p, (err) => {
+    if (err) throw err;
+  });
+}
 
 open({
-  filename: "./database.db",
+  filename: p,
   driver: Database,
-}).then((db) => {
-  console.log("got the DB");
+}).then(async (db) => {
+  await db.exec(`CREATE TABLE provider (
+    id INTEGER PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL
+  )`);
+  await db.exec(`CREATE TABLE patient (
+    id INTEGER PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    middle_name TEXT,
+    last_name TEXT NOT NULL,
+    dob TEXT NOT NULL,
+    status TEXT NOT NULL,
+    provider INTEGER,
+    FOREIGN KEY(provider) REFERENCES provider(id)
+  )`);
+  await db.exec(`CREATE TABLE address (
+    id INTEGER PRIMARY KEY,
+    line_1 TEXT NOT NULL,
+    line_2 TEXT,
+    city TEXT NOT NULL,
+    state TEXT NOT NULL,
+    zip TEXT NOT NULL,
+    patient INTEGER,
+    FOREIGN KEY(patient) REFERENCES patient(id)
+  )`);
 });
